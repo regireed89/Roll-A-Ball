cb525eee8ba74c3da281e80d8067c4826b80fd74
regireed89 <regireed89@gmail.com>
2017-04-25 18:56:43 -0500

    behaviors

--- Assets/Scripts/SteeringBehaviors.cs
+++ Assets/Scripts/SteeringBehaviors.cs
@@ -1,41 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SteeringBehaviors : MonoBehaviour 
 {
-	public static GameObject target;
-	static GameObject playerObject = GameObject.Find(target.name);
-	Vector3 playerPos = playerObject.transform.position;
-	Vector3 maxvelo = new Vector3 (10, 10, 10);
-	Vector3 velocity = new Vector3 (0, 0, 0);
+	public GameObject target;
+	Vector3 playerPos;
+	public int maxvelo = 10;
 	Vector3 dis = new Vector3 (0, 0, 0);
 	Vector3 norm = new Vector3 (0, 0, 0);
 	Vector3 scale = new Vector3 (0, 0, 0);
 	Vector3 force = new Vector3 (0, 0, 0);
+	Vector3 velocity = new Vector3 (0, 0, 0);
+	Vector3 acceleration = new Vector3 (0, 0, 0);
+	Vector3 heading = new Vector3 (0, 0, 0);
 	Rigidbody rb;
 	// Use this for initialization
 
-	Vector3 Seek()
-	{
-		dis = playerPos - gameObject.transform.position;
-		norm = dis.Normalize();
-		scale = norm.Scale(maxvelo);
-		force = scale - rb.velocity;
-		return force;
-	}
-
 	void Start() 
 	{
+		
 		rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetKeyDown("s")) 
-		{
-			rb.AddForce(Seek());
+		playerPos = target.transform.position;
+		if (Input.GetKey ("space")) {
+			dis = playerPos - gameObject.transform.position;
+			norm = dis.normalized;
+			scale = norm * maxvelo;
+			force = scale - velocity;
+			rb.AddForce (force, ForceMode.Force);
+		} 
+
+		else {
+			dis = gameObject.transform.position - playerPos;
+			norm = dis.normalized;
+			scale = norm * maxvelo;
+			force = scale - velocity;
+			rb.AddForce (force, ForceMode.Force);
 		}
+
+		acceleration = force;
+		heading = velocity.normalized;
+		velocity = heading * velocity.magnitude + acceleration * Time.deltaTime;
+		if (velocity.magnitude > maxvelo) 
+		{
+			velocity = velocity.normalized * maxvelo;
+		}
+		gameObject.transform.position = gameObject.transform.position + velocity * Time.deltaTime;
 	}
 
 }
